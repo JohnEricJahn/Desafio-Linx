1f381fff5030bd319947dacab219eeaa6056e347
FrostPriest <johnericjahn@gmail.com>
2020-07-17 14:57:02 -0300

    Removing Cofig From DataBase

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'J!frost123',
-  database: 'postgres',
+  host: '',
+  port: ,
+  username: '',
+  password: '',
+  database: '',
   define: {
     timestamps: true,
     underscored: true,
